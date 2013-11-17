7b8553af97f4c52565ea22a316cc5b3ce41d37da
seckcoder <seckcoder@gmail.com>
2013-11-17 22:56:26 +0800

    eopl 3.32. any number of mututally recursive unary procedures for letrec

--- racket/base/utils.rkt
+++ racket/base/utils.rkt
@@ -5,3 +5,21 @@
 (define anything?
   (lambda (v)
     #t))
+
+(define list-of
+  (lambda (pred)
+    (lambda (val)
+      (or (null? val)
+          (and (pair? val)
+               (pred (car val))
+               ((list-of pred) (cdr val)))))))
+
+
+(define index-of
+  (lambda (lst v)
+    (define (iter lst v idx)
+      (cond ((null? lst) -1)
+            ((eq? (car lst) v) idx)
+            (else
+              (iter (cdr lst) v (+ 1 idx)))))
+    (iter lst v 0)))
---
+++ racket/letrec-multi-declare/test.rkt
@@ -0,0 +1,5 @@
+#lang eopl
+
+(require "interp.rkt")
+
+(test)
