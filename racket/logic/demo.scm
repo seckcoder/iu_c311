fba4cf7160343be6c0784e061c74e7f571fbb525
seckcoder <seckcoder@gmail.com>
2013-12-17 23:41:03 +0800

    add gitignore

--- racket/logic/demo.scm
+++ racket/logic/demo.scm
@@ -16,3 +16,161 @@
 (run* (q)
   (twinso '(tofu tofu))
   (== #t q))
+
+
+
+(run 2 (q)
+  (membero 'e q))
+
+(run 1 (q)
+  (fresh (x l)
+    (membero x l)
+    (== q #t)))
+
+(run 2 (l)
+  (membero 'e l))
+
+(run* (q)
+  (fresh (x)
+    (pmembero x '(e e))
+    (== q #t)))
+
+
+(run* (q)
+  (memberrevo q '(1 2)))
+
+(run* (q)
+  (memo 'a '(c d a g b c a e) `(a . ,q)))
+
+(run 10 (q)
+  (fresh (u)
+    (memo 'tofu q u)))
+
+
+(run 1 (out)
+  (fresh (y)
+    (rembero 'peas `(a b ,y d peas e) out)))
+
+(run* (out)
+  (fresh (y z)
+    (rembero y `(a b ,y d ,z e) out)))
+
+(run* (out)
+  (rembero 'a '(a a) out))
+
+(run* (w)
+  (fresh (y z out)
+    (rembero y `(a b ,y d ,z . w) out)))
+
+(run* (v)
+  (== v 'b)
+  (surpriso v))
+
+(run 5 (q)
+  (fresh (y)
+    (appendo `(a . ,y) '(d e) q)))
+
+(run 5 (x)
+  (fresh (y)
+    (appendo x y '(cake with ice d t))))
+
+(run 2 (x)
+  (fresh (y)
+    (appendo x y '())))
+
+(run* (x)
+  (fresh (y z)
+    (conso x y z)))
+
+
+(run 7 (z)
+  (fresh (y x)
+    (appendo x y z)))
+
+(run 1 (z)
+  (fresh (x y)
+    (swappendo x y z)))
+
+(run 12 (x)
+  (unwrapo '(((pizza))) x))
+(run 2 (x)
+  (unwrapo `(,x) 'pizza))
+
+(run 1 (x)
+  (unwrapo `((,x)) 'pizza))
+
+(run 2 (l)
+  (flatteno '((a b) c) l))
+
+(run 3 (l)
+  (flatteno '(a) l))
+
+(run 2 (l)
+  (flatteno l '(a)))
+
+(run 5 (q)
+  alwayso
+  (== q #t))
+
+(run 2 (q)
+  (salo alwayso)
+  (== q #t))
+
+(run 1 (q)
+  alwayso
+  fail
+  (== q #t)
+  )
+
+(run 1 (q)
+  (condi
+    ((== #f q) alwayso)
+    (else (== #t q)))
+  (== #t q))
+
+(run 2 (q)
+  (== #t q))
+
+(run 2 (q)
+  (condi
+    (fail)
+    ((== #t q)))
+  (== #t q))
+
+(run 2 (q)
+  (condi
+    ((== #f q) alwayso)
+    (else ))
+  (== #t q))
+
+(run 5 (r)
+  (condi
+    ((teacupo r))
+    ((== #f r))))
+
+(define foo
+  (lambda ()
+    (conde
+      (fail)
+      (succeed))))
+
+(define foo-o (foo))
+
+(run 1 (q)
+  nevero)
+
+(run 2 (q)
+  (condi
+    (alwayso)
+    (foo-o)
+    )
+  (== #t q))
+
+(run 1 (q)
+  (alli
+    (conde
+      ((== #f q))
+      ((== #t q)))
+    alwayso
+    )
+  (== #t q))
--- racket/logic/utils.scm
+++ racket/logic/utils.scm
@@ -1,7 +1,13 @@
 ; This implementation of caro is wrong. Why?
 #|(define caro|#
-  ;(lambda (lst q)
+    ;(lambda (lst q)
     #|(== (car lst) q)))|#
+
+(define teacupo
+  (lambda (x)
+    (conde
+      ((== 'tea x))
+      ((== 'cup x)))))
 
 (define caro
   (lambda (lst a)
@@ -63,14 +69,184 @@
       (conso x '() y))))
 
 ; list of twins
+#|(define loto
+    (lambda (l)
+      (conde
+        ((nullo l))
+        ((fresh (a)
+           (caro l a)
+           (twinso a))
+         (fresh (d)
+           (cdro l d)
+           (loto d)))
+        )))|#
+
 (define loto
   (lambda (l)
+    (listofo twinso l)))
+
+(define listofo
+  (lambda (predo l)
     (conde
       ((nullo l))
       ((fresh (a)
          (caro l a)
-         (twinso a))
+         (predo a))
        (fresh (d)
          (cdro l d)
-         (loto d)))
+         (listofo predo d))))))
+
+(define eq-caro
+  (lambda (l x)
+    (caro l x)))
+
+(define membero
+  (lambda (x l)
+    (conde
+      ; whenever a conde line is guaranteed to
+      ; fail, it's unnecessary. But it's better
+      ; to leave it for documenting purpose
+      ; ((nullo l) fail)
+      ((eq-caro l x))
+      ((fresh (d)
+         ; the order influences the result.
+         ; swap the order of cdro and membero
+         ; the program will hang there for some cases of run*
+         ; . Why?...
+         (cdro l d)
+         (membero x d)
+         )))))
+
+(define pmembero
+  (lambda (x l)
+    (conde
+      ; whenever a conde line is guaranteed to
+      ; fail, it's unnecessary. But it's better
+      ; to leave it for documenting purpose
+      ; ((nullo l) fail)
+      ((eq-caro l x)
+       (cdro l '()))
+      ((eq-caro l x)
+       (fresh (a d)
+         (cdro l (cons a d))))
+      ((fresh (d)
+         (cdro l d)
+         (pmembero x d))))))
+
+; apparently, this doens't work...
+(define memberrevo
+  (lambda (x l)
+    (conde
+      ((fresh (d)
+         (cdro l d)
+         (memberrevo x d)))
+      ((eq-caro l x)))))
+
+(define memo
+  (lambda (x l out)
+    (conde
+      ((eq-caro l x)
+       (== l out))
+      ((fresh (d)
+         (cdro l d)
+         (memo x d out)))
       )))
+
+; remove a member x or not remove
+(define rembero
+  (lambda (x l out)
+    (conde
+      ((nullo l) (== out '()))
+      ((eq-caro l x)
+       (cdro l out))
+      ((fresh (d d-r a)
+         ; Note here we didn't request a != x
+         (conso a d l)
+         (rembero x d d-r)
+         (conso a d-r out))))))
+
+(define surpriso
+  (lambda (r)
+    ; The problem with the supriso
+    ; is that rembero could pass even if we don't remove x
+    ; when x equals b. To solve
+    ; the problem, we need introduce negation operator
+    ; and request a != x in the last condition.
+    (rembero r '(a b c) '(a b c))))
+
+; append a list l with s into out
+; Note l should be a list according to the definition,
+; but s should not have to be.
+(define appendo
+  (lambda (l s out)
+    (conde
+      ((nullo l) (== out s))
+      ((fresh (a d res)
+         (conso a d l)
+         ;!!!
+         (conso a res out)
+         (appendo d s res)
+         )))))
+
+(define swappendo
+  (lambda (l s out)
+    (conde
+      ((fresh (a d res)
+         (conso a d l)
+         (conso a res out)
+         (swappendo d s res)))
+      ((nullo l) (== out s)))))
+
+(define unwrapo
+  (lambda (x out)
+    (conde
+      ((== x out))
+      ((pairo x)
+       (fresh (a)
+         (caro x a)
+         (unwrapo a out)))
+      )))
+
+(define flatteno
+  (lambda (s out)
+    (conde
+      ((nullo s)
+       (== out s))
+      ((conso s '() out))
+      ((fresh (a d res1 res2)
+         (conso a d s)
+         (flatteno a res1)
+         (flatteno d res2)
+         (appendo res1 res2 out)))
+      )))
+
+(define anyo
+  (lambda (g)
+    (conde
+      (g)
+      (else (anyo g)))))
+
+(define nevero (anyo fail))
+
+(define alwayso (anyo succeed))
+
+(define salo
+  (lambda (g)
+    (conde
+      (succeed)
+      (g))))
+
+(define identify
+  (lambda (l)
+    (fresh (q)
+      (membero q l))))
+
+(define first-value
+  (lambda (l)
+    (run 1 (y)
+      (membero y l))))
+
+(define reverse-list
+  (lambda (l)
+    (run* (y)
+      (memberrevo y l))))
